27ea8b3ae47d7a783005bc24936564c774ab0d89
Jeff <jeff.revesz@buzzfeed.com>
2014-03-03 11:06:34 -0500

    added basic libs

---
+++ moppy_python/lib/floppy.py
@@ -0,0 +1,50 @@
+from __future__ import absolute_import
+
+import time
+
+from .. import settings
+from .arduino import Pin
+
+class Floppy(object):
+    """
+    Represents a floppy drive connected to the Arduino
+
+    """
+
+    def __init__(self, id, step_pin_number, direction_pin_number):
+        """
+        Initializes the Floppy Drive
+
+        """
+        self.id = id
+        self.step_pin = Pin(step_pin_number)
+        self.direction_pin = Pin(direction_pin_number)
+
+    def drive(self, direction, cycles, speed=settings.DEFAULT_SPEED):
+        """
+        Steps the motor a specified number of times in any direction
+
+        """
+        if speed > settings.MAX_SPEED:
+            raise Exception("Fatal Error. Tried to drive board %s at %s Hz. This exceeds the maximum rate of %s Hz" % (self.id, speed, settings.MAX_SPEED))
+
+        self.direction_pin.set_state(direction)
+        for x in xrange(cycles):
+            print "STEPPING DRIVE %s IN DIRECTION %s" % (self.id, direction)
+            self.step_pin.pulse()
+            time.sleep(float(1) / speed)
+
+class FloppyArray(object):
+    """
+    Represents the array of floppy drives connected
+
+    """
+
+    def __init__(self):
+        """
+        Initializes the array
+
+        """
+        self.floppy_drives = []
+        for x in xrange(2,(settings.FLOPPY_DRIVE_COUNT * 2) + 2, 2):
+            self.floppy_drives.append(Floppy(len(self.floppy_drives), x, x + 1))
---
+++ moppy_python/lib/utils.py
@@ -0,0 +1,24 @@
+import glob
+import serial
+import platform
+
+# A function that tries to list serial ports on most common platforms
+def list_serial_ports():
+    system_name = platform.system()
+    if system_name == "Windows":
+        # Scan for available ports.
+        available = []
+        for i in range(256):
+            try:
+                s = serial.Serial(i)
+                available.append(i)
+                s.close()
+            except serial.SerialException:
+                pass
+        return available
+    elif system_name == "Darwin":
+        # Mac
+        return glob.glob('/dev/tty*') + glob.glob('/dev/cu*')
+    else:
+        # Assume Linux or something else
+        return glob.glob('/dev/ttyS*') + glob.glob('/dev/ttyUSB*')
